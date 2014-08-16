ecb5fe7223aee8e7b1fa29e5be535b7342e41285
NTF-32 <ntf-32@NTF-32.(none)>
2014-08-16 13:34:07 +0530

    d

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:name, :content, :author_name))
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
-    end    
+    
   end
 
   def show
